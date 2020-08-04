30756b242f2f7dff5ad53a2c366b0e90fd92106a
dtadmor <bucsno1@gmail.com>
2020-08-04 13:03:33 -0400

    Format functions for readability
    Format spacing and variable names for readability. A few other minor changes.

--- level_3/grandest_staircase/solution.py
+++ level_3/grandest_staircase/solution.py
@@ -1,16 +1,17 @@
-import argparse
-
 def explore(bricks, last_step, solution_map):
+
     brick_solutions = solution_map.get(bricks)
     if isinstance(brick_solutions, dict):
-        n_solutions = brick_solutions.get(last_step)
-        if isinstance(n_solutions, int):
-            return n_solutions
+        n_options = brick_solutions.get(last_step)
+        if isinstance(n_options, int):
+            return n_options
+
     max_split = (bricks - 1) // 2
     if last_step >= max_split:
-        if bricks <= last_step:
-            print('ERROR')
-        return 1
+        assert bricks > last_step, 'Not enough bricks left!'
+        n_options = 1
+        return n_options
+
     if last_step > 0:
         n_options = 1
     else:
@@ -19,19 +20,16 @@ def explore(bricks, last_step, solution_map):
         n_options += explore(
             bricks=bricks-step, last_step=step, solution_map=solution_map
         )
+
     if not isinstance(solution_map.get(bricks), dict):
         solution_map[bricks] = {}
     solution_map[bricks][last_step] = n_options
+
     return n_options
 
 
 def solution(n):
-    n_options = explore(bricks=n, last_step=0, solution_map={})
-    return n_options
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('bricks', type=int)
-    args = parser.parse_args()
-    n = args.bricks
-    print(solution(n))
+    n_options = explore(bricks=n, last_step=0, solution_map={})
+
+    return n_options
--- utils/tests.py
+++ utils/tests.py
@@ -1,11 +1,14 @@
 def gen_test(func, examples):
-    
+
     for i, case in examples.items():
+
         output = func(*case['args'])
         success = case['answer'] == output
+
         msg = 'Case {}; Arguments {}; Success {}'.format(
             i, case['args'], success
         )
         if not success:
             msg += '; Output {}; Answer {}'.format(output, case['answer'])
+            
         print(msg)
